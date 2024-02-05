8d20c714d6ca535d9a013356e840fa93240a70de
AnnaLinden <linden.a.anna@gmail.com>
2024-02-05 19:23:39 +0200

    backend login works and tested with postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,8 +11,18 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-    
-    res.json({ message: 'login user' })
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create token
+        const token = createToken(user._id)
+        res.status(200).json( {email, token} )
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 }
 // signup user
 const signupUser = async (req, res) => {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,6 +14,12 @@ const userSchema = new Schema({
   //static signup method
     userSchema.statics.signup = async function (firstName, lastName, email, password) {
 
+        //validation
+        if (!firstName || !lastName || !email || !password) {
+            throw new Error('All fields are required');
+        }
+
+
         const exists = await this.findOne({ email });
         if (exists) {
             throw new Error('User already exists');
@@ -25,6 +31,25 @@ const userSchema = new Schema({
         const user = await this.create({ firstName, lastName, email, password: hash});
 
         return user;
+    };
+
+    //static login method
+    userSchema.statics.login = async function (email, password) {
+
+        //validation
+        if (!email || !password) {
+            throw new Error('All fields are required');
+        }
+
+        const user = await this.findOne({ email });
+        if (user) {
+            const auth = await bcrypt.compare(password, user.password);
+            if (auth) {
+                return user;
+            }
+            throw new Error('Incorrect password');
+        }
+        throw new Error('Incorrect email');
     };
 
   const User = mongoose.model('User', userSchema);
