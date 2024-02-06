b1c69e038e5caf1d76648c75f628987dbf6f40c9
AnnaLinden <linden.a.anna@gmail.com>
2024-02-06 14:39:29 +0200

    useSignup

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -1,3 +1,43 @@
 import {useState} from 'react';
 import {useAuthContext} from '../context/AuthContext';
 
+export const useSignup = () => {
+    const {dispatch} = useAuthContext();
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+
+    const signup = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch('/api/user/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, password}),
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setError(json.message);
+            setIsLoading(false);
+        }
+
+        if (response.ok) {
+           localStorage.setItem('user', JSON.stringify(json.user));
+
+            dispatch({type: 'LOGIN', payload: json.user});
+
+            setIsLoading(false);
+        }
+    };
+
+    return {signup, error, isLoading};
+}
+
+
+
+
+
