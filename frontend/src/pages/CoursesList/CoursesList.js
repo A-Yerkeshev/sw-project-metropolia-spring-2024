7d8a301769c81168d9549b3cc4ae05700d6d5273
Arman Yerkesh <a.yerkesh@gmail.com>
2024-02-15 10:22:04 +0200

    Add courses table

--- frontend/src/pages/CoursesList/CoursesList.js
+++ frontend/src/pages/CoursesList/CoursesList.js
@@ -1,6 +1,48 @@
 import styles from './CoursesList.module.css';
+import { useState, useEffect } from "react";
 
 const CoursesList = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        // const url = process.env.REACT_APP_BACKEND_URL + '/api/courses';
+
+        // const fetchCourses = async () => {
+        //   const res = await fetch(url);
+        //   const courses = await res.json();
+
+        //   setCourses(courses);
+        //   console.log(courses);
+        // }
+
+        // fetchCourses();
+
+        const courses = [
+            {
+                _id: '2874682101',
+                name: 'Software 1',
+                students: [1,2,3],
+                teachers: [{
+                    firstName: 'Juso',
+                    lastName: 'Pehkonen'
+                }]
+            }, {
+                _id: '2874682102',
+                name: 'Software 2',
+                students: [1,2,3,4,5],
+                teachers: [{
+                    firstName: 'Jarkko',
+                    lastName: 'Punavuori'
+                }, {
+                    firstName: 'Asko',
+                    lastName: 'Mattila'
+                }]
+            },
+        ];
+
+        setCourses(courses);
+    }, []);
+
     return(
         <div className={styles.main}>
             <div className={styles.headerTextContainer}>
@@ -26,6 +68,28 @@ const CoursesList = () => {
                     <p className={styles.actionButtonGroupText}>Group</p>
                 </div>
             </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Students</th>
+                        <th>Teachers</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                {courses.map((course) => {
+                    return  <tr key={course._id}>
+                                <td>{course.name}</td>
+                                <td>{course.students.length}</td>
+                                <td>{course.teachers.map((teacher) => teacher.firstName + ' ' + teacher.lastName).join(', ')}</td>
+                                <td>
+                                    <a className={styles.courseBtn} href={`/courses/${course._id}`}>Go to course</a>
+                                </td>
+                            </tr>
+                })}
+                </tbody>
+            </table>
         </div>
     )
 }
