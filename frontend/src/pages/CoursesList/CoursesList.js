e7d87f4c7cd0651dec4edd0dee50113feb400c4f
AnnaLinden <linden.a.anna@gmail.com>
2024-02-25 20:46:09 +0200

    minor typo corrections

--- frontend/src/pages/CoursesList/CoursesList.js
+++ frontend/src/pages/CoursesList/CoursesList.js
@@ -46,8 +46,8 @@ const CoursesList = () => {
     return(
         <div className={styles.main}>
             <div className={styles.headerTextContainer}>
-                <p className={styles.headerText}>You’re Courses List</p>
-                <p className={styles.subHeaderText}>All courses that been signed to you’re account in 30 days.</p>
+                <p className={styles.headerText}>Your Courses List</p>
+                <p className={styles.subHeaderText}>All courses that been signed to your account in 30 days.</p>
             </div>
             <div className={styles.searchButtonContainer}>
                     <div className={styles.searchButtonText}>
