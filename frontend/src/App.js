e5e5d6fafcdde3af60c7824ce37d7db00d4af068
AnnaLinden <linden.a.anna@gmail.com>
2024-03-01 10:21:52 +0200

    navbar is not rendered in some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,35 @@
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import {useAuthContext} from './hooks/useAuthContext';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // pages & components
-import Signup from './pages/Login/Signup';
-import Login from './pages/Login/Login';
-import QR from './pages/QR';
-import Feedback from './pages/Feedback/Feedback';
-import Navbar from './components/Navbar/Navbar';
-import CoursesList from './pages/CoursesList/CoursesList';
-import Course from './pages/Course/Course';
-import Users from './pages/Users/Users';
+import Signup from "./pages/Login/Signup";
+import Login from "./pages/Login/Login";
+import QR from "./pages/QR";
+import Feedback from "./pages/Feedback/Feedback";
+import Navbar from "./components/Navbar/Navbar";
+import CoursesList from "./pages/CoursesList/CoursesList";
+import Course from "./pages/Course/Course";
+import Users from "./pages/Users/Users";
 
 function App() {
-  const {user} = useAuthContext();
+  const { user } = useAuthContext();
 
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <Routes>
-          <Route path="/" element= {<Login />} />
-          <Route path="/signup" element= {!user ? <Signup /> : <Navigate to="/" />} />
-          <Route path="/share" element= {<QR />}/>
-          <Route path="/feedback/new" element= {<Feedback />}/>
-          <Route path="/CoursesList" element= {<CoursesList />} />
-          <Route path="/Course" element= {<Course />} />
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
+          <Route path="/share" element={<QR />} />
+          <Route path="/feedback/new" element={<Feedback />} />
+          <Route path="/CoursesList" element={<CoursesList />} />
+          <Route path="/Course" element={<Course />} />
           <Route path="/courses/:courseId" element={<Course />} />
-          <Route path="/Users" element= {<Users />} />
+          <Route path="/Users" element={<Users />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,50 +1,55 @@
-import { Link } from 'react-router-dom';
-import { useAuthContext } from '../../hooks/useAuthContext'; 
-import styles from './Navbar.module.css';
-import  {useLogout} from '../../hooks/useLogout'
+import { Link, useLocation } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import styles from "./Navbar.module.css";
+import { useLogout } from "../../hooks/useLogout";
 
 const Navbar = () => {
-    const { user } = useAuthContext();
-    const {logout} = useLogout();
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+  const location = useLocation();
 
-    console.log('Navbar user:', user);
+  console.log("Navbar user:", user);
 
-    if (!user) {
-        return null;
-    }
+  // Define the routes where the Navbar should not be displayed
+  const hideOnRoutes = ["/", "/signup", "/share", "/feedback/new"];
 
-    return (
-        <div className={styles.mainNavBarContainer}>
-            <div className={styles.topMenu}> 
-                <div className={styles.topMenuEmail}>
-                    <div className={styles.topMenuEmailAvatar}>   
-                    </div>
-                    <p>{user.email}</p>
-                </div>
-                <div className={styles.topMenuUserAvatar}>
-                </div>
-                <button onClick={logout} className={styles.logoutButton}>Logout</button>
-            </div>           
-            <div className={styles.bottonMenu}>
-                <Link to="/CoursesList" className={styles.bottonMenuContainer}>
-                <img src="/navbar/CoursesList.svg" alt="course" />
-                <p>Courses List</p>
-                </Link>
-                <Link to="/Course" className={styles.bottonMenuContainer}>
-                <img src="/navbar/Course.svg" alt="course" />
-                    <p>Course</p>
-                </Link>
-                <Link to="/Users" className={styles.bottonMenuContainer}>
-                <img src="/navbar/Users.svg" alt="course" />
-                    <p>Users</p>
-                </Link>
-                <Link to="/Settings" className={styles.bottonMenuContainer}>
-                <img src="/navbar/Settings.svg" alt="course" />
-                    <p>Setttings</p>
-                </Link>
-            </div>
+  // Check if the current route is in the list of routes to hide the Navbar on
+  if (!user || hideOnRoutes.includes(location.pathname)) {
+    return null;
+  }
+
+  return (
+    <div className={styles.mainNavBarContainer}>
+      <div className={styles.topMenu}>
+        <div className={styles.topMenuEmail}>
+          <div className={styles.topMenuEmailAvatar}></div>
+          <p>{user.email}</p>
         </div>
-    );
-}
+        <div className={styles.topMenuUserAvatar}></div>
+        <button onClick={logout} className={styles.logoutButton}>
+          Logout
+        </button>
+      </div>
+      <div className={styles.bottonMenu}>
+        <Link to="/CoursesList" className={styles.bottonMenuContainer}>
+          <img src="/navbar/CoursesList.svg" alt="course" />
+          <p>Courses List</p>
+        </Link>
+        <Link to="/Course" className={styles.bottonMenuContainer}>
+          <img src="/navbar/Course.svg" alt="course" />
+          <p>Course</p>
+        </Link>
+        <Link to="/Users" className={styles.bottonMenuContainer}>
+          <img src="/navbar/Users.svg" alt="course" />
+          <p>Users</p>
+        </Link>
+        <Link to="/Settings" className={styles.bottonMenuContainer}>
+          <img src="/navbar/Settings.svg" alt="course" />
+          <p>Setttings</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
