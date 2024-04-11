9abef8b6cd660b5a4aadcb8f2161c7cb8412ae58
Arman Yerkesh <a.yerkesh@gmail.com>
2024-04-11 15:46:13 +0300

    Remove language detector

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -12,9 +12,6 @@ i18n
   // learn more: https://github.com/i18next/i18next-http-backend
   // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
