d7da41cba593beaf160ec46ce06281ee1b4621a9
Arman Yerkesh <a.yerkesh@gmail.com>
2024-04-13 10:39:12 +0300

    Bring back LanguageDetector

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -7,18 +7,15 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // have a look at the Quick start guide
 // for passing in lng and translations on init
 
-i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  .use(Backend)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-  });
+i18n.use(Backend);
+i18n.use(LanguageDetector).init({
+  detection: {
+    order: ['querystring', 'cookie', 'localStorage', 'sessionStorage'],
+  },
+});
+i18n.use(initReactI18next).init({
+  fallbackLng: 'en',
+  debug: true,
+});
 
 export default i18n;
