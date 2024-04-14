98df0cb84f059a3d1ca38abad6cbb54fd78e79c8
arturgola <artur.golavskiy@metropolia.fi>
2024-04-15 01:43:29 +0300

    linkedin link to a developer card.

--- frontend/src/components/BasicCard.js
+++ frontend/src/components/BasicCard.js
@@ -1,12 +1,13 @@
-
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-const BasicCard = ({ title, content, actionLink, imageUrl }) => {
+const BasicCard = ({ title, content, actionLink, linkedInLink, imageUrl }) => {
   return (
     <Card sx={{ minWidth: 275, backgroundColor: '#F1F1F1' }}>
       <CardContent>
@@ -23,8 +24,13 @@ const BasicCard = ({ title, content, actionLink, imageUrl }) => {
       {actionLink && (
         <CardActions>
           <a href={actionLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-            <Button size="small">GitHub</Button>
+            <Button size="small" startIcon={<GitHubIcon />}>GitHub</Button>
           </a>
+          {linkedInLink && (
+            <a href={linkedInLink} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+              <Button size="small" startIcon={<LinkedInIcon />}>LinkedIn</Button>
+            </a>
+          )}
         </CardActions>
       )}
     </Card>
@@ -32,4 +38,3 @@ const BasicCard = ({ title, content, actionLink, imageUrl }) => {
 };
 
 export default BasicCard;
-
