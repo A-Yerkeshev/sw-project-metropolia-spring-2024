1c147030d3eb80f84582fd5c0a7617f01c820334
dungdpham <dung.pham3@metropolia.fi>
2024-04-15 09:47:56 +0300

    add Vietnamese translations

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import { useLogin } from "../../hooks/useLogin";
-import { useNavigate } from "react-router-dom";
-import { Link as RouterLink } from "react-router-dom";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { useTranslation } from "react-i18next";
-import StudentNavbar from "../../components/Navbar/StudentNavbar";
+import React, { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
+import { useNavigate } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { useTranslation } from 'react-i18next';
+import StudentNavbar from '../../components/Navbar/StudentNavbar';
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [formError, setFormError] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [formError, setFormError] = useState('');
   const { login, error, isLoading } = useLogin();
   const navigate = useNavigate();
   const { t, i18n } = useTranslation();
@@ -26,19 +26,19 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Reset form error state
-    setFormError("");
+    setFormError('');
 
     // Basic validation for empty fields
     if (!email.trim() || !password.trim()) {
-      setFormError(t("login.emailAndPasswordRequired")); // Set an error message
+      setFormError(t('login.emailAndPasswordRequired')); // Set an error message
       return; // Prevent the form from being submitted
     }
 
     try {
       await login(email, password);
-      navigate("/Courses");
+      navigate('/Courses');
     } catch (err) {
-      console.error("Failed to login:", err);
+      console.error('Failed to login:', err);
     }
   };
 
@@ -50,16 +50,16 @@ const Login = () => {
         <Box
           sx={{
             marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            {t("login.header")}
+            {t('login.header')}
           </Typography>
           <Box
             component="form"
@@ -72,7 +72,7 @@ const Login = () => {
               required
               fullWidth
               id="email"
-              label={t("login.emailAddressLabel")}
+              label={t('login.emailAddressLabel')}
               name="email"
               autoComplete="email"
               autoFocus
@@ -84,7 +84,7 @@ const Login = () => {
               required
               fullWidth
               name="password"
-              label={t("login.passwordLabel")}
+              label={t('login.passwordLabel')}
               type="password"
               id="password"
               autoComplete="current-password"
@@ -98,16 +98,16 @@ const Login = () => {
               sx={{ mt: 3, mb: 2 }}
               disabled={isLoading}
             >
-              {t("login.loginButton")}
+              {t('login.loginButton')}
             </Button>
             {error && <Typography color="error">{error}</Typography>}
             {formError && <Typography color="error">{formError}</Typography>}
 
             <Grid container>
               <Grid item>
-                <RouterLink to="/signup" style={{ textDecoration: "none" }}>
+                <RouterLink to="/signup" style={{ textDecoration: 'none' }}>
                   <Typography variant="body2">
-                    {t("login.signUpPrompt")}
+                    {t('login.signUpPrompt')}
                   </Typography>
                 </RouterLink>
               </Grid>
