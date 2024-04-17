9c315e8a5e25823557c210d08b3caf9748853827
Arman Yerkesh <a.yerkesh@gmail.com>
2024-04-17 10:47:03 +0300

    Configure playwright to start servers before executing tests

--- backend/playwright.config.ts
+++ backend/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -69,9 +69,22 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: [
+    {
+      command: 'npm run dev',
+      url: `http://127.0.0.1:${process.env.PORT || 4000}`,
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      cwd: '../frontend',
+      command: 'npm start',
+      url: 'http://127.0.0.1:3000',
+      reuseExistingServer: !process.env.CI,
+      timeout: 10000,
+      env: {
+        REACT_APP_BACKEND_URL: `http://127.0.0.1:${process.env.PORT || 4000}`
+      }
+    }
+  ],
 });
