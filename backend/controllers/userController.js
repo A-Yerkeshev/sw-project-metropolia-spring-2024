1dd57d3438f62c15bfc6bad30a183415b78cfa95
Arman Yerkesh <a.yerkesh@gmail.com>
2024-04-19 16:33:38 +0300

    Add resetPassword function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 const { User } = require('../models/userModel');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const i18next = require('../i18n');
+const bcrypt = require('bcrypt');
 secret = process.env.JWT_SECRET;
 
 const createToken = (_id) => {
@@ -55,4 +57,34 @@ const signupUser = async (req, res) => {
   }
 };
 
-module.exports = { signupUser, loginUser };
+const resetPassword = async (req, res) => {
+  const { token, email, password, passwordRep, language } = req.body;
+
+  if (!token || !email || !password || !passwordRep) {
+    return res.status(400).json({ error: i18next.t('resetPassword.missingFields', { lng: language }) });
+  }
+
+  if (password !== passwordRep) {
+    return res.status(400).json({ error: i18next.t('resetPassword.passwordsMismatch', { lng: language }) });
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    return res.status(404).json({ error: i18next.t('resetPassword.wrongEmail', { lng: language }) });
+  }
+
+  if (user.recoveryToken !== token) {
+    return res.status(400).json({ error: i18next.t('resetPassword.recoveryFailed', { lng: language }) });
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    user.password = hash;
+    return res.status(200).json({ error: i18next.t('resetPassword.recoveryOk', { lng: language }) });
+  } catch {
+    return res.status(500).json({ error: i18next.t('resetPassword.recoveryFailed', { lng: language }) });
+  }
+}
+
+module.exports = { signupUser, loginUser, resetPassword };
